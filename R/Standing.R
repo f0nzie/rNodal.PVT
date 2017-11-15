ea53f985d039775d99e17216a2107e845a29361f
fonzie zbook <alfonso.reyes@oilgainsanalytics.com>
2017-11-15 06:12:32 -0800

    add unit test for Standing Bo saturated. add roxygen2 comments.

--- R/Standing.R
+++ R/Standing.R
@@ -1,12 +1,21 @@
 
 #' @title Bubble Point Pressure by Standing
 #' @description Pb Standing Correlation
+#' @param oil.Rsi solution oil gas ratio
+#' @param gas.sg gas specific gravity
+#' @param oil.API oil specific gravity in API degrees
+#' @param fluid.tempF fluid temperature in Farenheit
+#' @rdname Standing
 #' @export
 Pob.Standing <- function(oil.Rsi, gas.sg, oil.API, fluid.tempF) {
     18.2 * ((oil.Rsi/gas.sg)^0.83 * 10^(0.00091 * fluid.tempF) / 10^(0.0125 * oil.API) - 1.4)
 }
 
 
+#' @title Formation Volume Factor by Standing
+#' @description Bo Standing Correlation
+#' @inheritParams Pob.Standing
+#' @export
 Bo.Standing <- function(oil.Rsi, gas.sg, oil.API, fluid.tempF) {
     oil.sg <- 141.5 / (oil.API + 131.5)
     0.972 + 1.47 *10^-4 * (oil.Rsi^(gas.sg/oil.sg) + 1.25 * fluid.tempF)^1.175
--- tests/testthat/test_standing_pb.R
+++ tests/testthat/test_standing_pb.R
@@ -1,3 +1,7 @@
 library(testthat)
 
+context("Standing correlations")
+
 expect_equal(Pob.Standing(768, 0.855, 40.7, 220), 2502.882, tolerance = 1e-6)
+
+expect_equal(Bo.Standing(768, 0.855, 40.7, 220), 1.630176, tolerance = 1e-6)
